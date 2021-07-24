421ec628f7305d0c25c49b081d756ed9ac55ab4e
Claudio Delgado <claud.app@gmail.com>
2021-07-24 14:55:09 +0700

    Publishing on v8blog.git

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:innoktive/hello-world.git master:gh-pages
+git push -f git@github.com:innoktive/v8blog.git master:gh-pages
 cd -
